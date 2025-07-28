df39a3f64c440585ddb7a2345fdc0c39d58e75cf
rzkypribadi <rzkyalamak@gmail.com>
2025-07-28 23:02:50 +0700

    firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,16 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyANSYmNZHRuFE6p6lga3cEkYTCQU67xT7g",
+  authDomain: "database-invest.firebaseapp.com",
+  databaseURL: "https://database-invest-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "database-invest",
+  storageBucket: "database-invest.firebasestorage.app",
+  messagingSenderId: "465429517765",
+  appId: "1:465429517765:web:de893fc118784569ef67ac",
+  measurementId: "G-XW756RZPZJ"
+};
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+const database = firebase.database();
